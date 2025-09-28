efbff841ba64a260e0f342b8809f275054c84313
Robert Gleason <l2ob122@gmail.com>
2025-09-28 12:48:52 -0700

    Refactor main.rs to improve variable naming and organization showing how mutable and immutable variables work in Rust.

--- variables-and-mutability/src/main.rs
+++ variables-and-mutability/src/main.rs
@@ -1,7 +1,21 @@
 fn main() {
+    //  Variables
+
     let dvd = 50;
     let vhs = 11;
-    let media_owned = dvd + vhs;
+    let _media_owned = dvd + vhs;
 
-    println!("The total amount of media i am selling is {media_owned}.",)
+    //     println!("The total amount of media i am selling is {media_owned}.",)
+    println!("This year i bought {0} dvds and {1} vhs tapes", dvd, vhs);
+
+    //    Immutable and Mutable variables
+    let gym_reps = 10;
+    println!("I plan to do {gym_reps} reps");
+
+    //     gym_reps = 15; this wont work
+
+    let mut buy_movie = 11;
+    println!("I plan on buying {buy_movie} movies today");
+    buy_movie = 7;
+    println!("Decided to get {buy_movie} more movies today");
 }
