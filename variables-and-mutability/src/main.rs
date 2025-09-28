5c8493d3119d2ce274df872d3379d5f5e1c6c672
Robert Gleason <l2ob122@gmail.com>
2025-09-28 15:47:24 -0700

    Refactor main.rs and add variables-mutability.rs for improved organization and clarity in variable usage examples

--- variables-and-mutability/src/main.rs
+++ variables-and-mutability/src/main.rs
@@ -1,67 +1,51 @@
-//compiler directives
-#![allow(unused_variables)]
+/*
+Declare a `season` variable set to a string with
+your favorite season. Provide an explicit type annotation.
+The type of a string is a `&str`. We'll discuss what
+the & symbol means later in the course.
 
-// Constant
-const TAX_RATE: f64 = 7.25;
+Declare a `points_scored` variable set to 28.
+Provide an explicit type annotation. The type of
+an integer is `i32`.
 
-// Type alias
-type Meters = i32;
+It's time to update the team's score. Declare the
+`points_scored` variable to be mutable. Set its
+new value to 35.
+
+Declare a `TOUCHDOWN_POINTS` constant at the file
+level set to the value 6.
+
+Declare a `event_time` variable set to a string of
+"06:00".
+
+Use variable shadowing to redeclare `event_time` set
+to a integer of 6.
+
+Use interpolation to print out all of the
+declared variables and constants in a println! call.
+Practice with direct interpolation, sequential
+arguments, and numeric arguments.
+
+Declare a `favorite_beverage` variable set to a string
+of your favorite drink. Use an underscore to silence
+the compiler warning about the variable being unused.
+
+Remove the underscore. Provide a compiler directive
+to silence the compiler warning about the variable
+being unused.
+*/
+
+const TOUCHDOWN_POINTS: i32 = 6;
 
 fn main() {
-    //  Variables
+    let season: &str = "Summer";
+    let mut points_scored: i32 = 35;
+    points_scored = 28;
 
-    let dvd = 50;
-    let vhs = 11;
-    let _media_owned = dvd + vhs;
+    let _event_time = "06:00";
+    let event_time: i32 = 6;
 
-    //     println!("The total amount of media i am selling is {media_owned}.",)
-    println!("This year i bought {0} dvds and {1} vhs tapes", dvd, vhs);
-
-    //    Immutable and Mutable variables
-    let gym_reps = 10;
-    println!("I plan to do {gym_reps} reps");
-
-    //     gym_reps = 15; this wont work
-
-    let mut buy_movie = 11;
-    println!("I plan on buying {buy_movie} movies today");
-    buy_movie = 7;
-    println!("Decided to get {buy_movie} more movies today");
-
-    // variable shadowing
-
-    let _grams_of_protein = "100.345";
-    let _grams_of_protein = 100.345;
-    let _grams_of_proten = 100;
-
-    // Scope
-
-    //  outer
-    let coffee_price = 5.99;
-
-    {
-        // inner
-        println!("The price is {coffee_price}");
-        let cookie_price = 1.99;
-        println!("The cookie price is {cookie_price}");
-        let coffee_price = 3.99;
-        println!("The price is {coffee_price}");
-    }
-
-    // Constant
-    let income = 10000;
-    println!("The tax rate is {TAX_RATE}");
-    println!("My income in {income} and my tax rate is {TAX_RATE}");
-
-    // Type alias
-
-    let mile_race_length: Meters = 1600;
-    let two_mile_race_length: Meters = 3200;
     println!(
-        "a mile long race is {mile_race_length} meters long and a two mile race is {two_mile_race_length}"
+        "In the {season} time there was {points_scored} over the course of 6 weeks and it ended at {event_time}",
     );
-
-    // Compiler directives
-    #[allow(unused_variables)]
-    let mighty_ducks_win = 66;
 }
---
+++ variables-and-mutability/src/variables-mutablity.rs
@@ -0,0 +1,67 @@
+//compiler directives
+#![allow(unused_variables)]
+
+// Constant
+const TAX_RATE: f64 = 7.25;
+
+// Type alias
+type Meters = i32;
+
+fn main() {
+    //  Variables
+
+    let dvd = 50;
+    let vhs = 11;
+    let _media_owned = dvd + vhs;
+
+    //     println!("The total amount of media i am selling is {media_owned}.",)
+    println!("This year i bought {0} dvds and {1} vhs tapes", dvd, vhs);
+
+    //    Immutable and Mutable variables
+    let gym_reps = 10;
+    println!("I plan to do {gym_reps} reps");
+
+    //     gym_reps = 15; this wont work
+
+    let mut buy_movie = 11;
+    println!("I plan on buying {buy_movie} movies today");
+    buy_movie = 7;
+    println!("Decided to get {buy_movie} more movies today");
+
+    // variable shadowing
+
+    let _grams_of_protein = "100.345";
+    let _grams_of_protein = 100.345;
+    let _grams_of_proten = 100;
+
+    // Scope
+
+    //  outer
+    let coffee_price = 5.99;
+
+    {
+        // inner
+        println!("The price is {coffee_price}");
+        let cookie_price = 1.99;
+        println!("The cookie price is {cookie_price}");
+        let coffee_price = 3.99;
+        println!("The price is {coffee_price}");
+    }
+
+    // Constant
+    let income = 10000;
+    println!("The tax rate is {TAX_RATE}");
+    println!("My income in {income} and my tax rate is {TAX_RATE}");
+
+    // Type alias
+
+    let mile_race_length: Meters = 1600;
+    let two_mile_race_length: Meters = 3200;
+    println!(
+        "a mile long race is {mile_race_length} meters long and a two mile race is {two_mile_race_length}"
+    );
+
+    // Compiler directives
+    #[allow(unused_variables)]
+    let mighty_ducks_win = 66;
+}
